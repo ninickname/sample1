f68099ddd04dbb96ee67f5c76a992fc9caa0d3c0
Nick <yashaev@gmail.com>
2015-08-06 08:41:22 +0300

    INIT
    workspace, tomcat and server initaalizations

---
+++ sample1/src/main/java/com/sample1/WebAppConfig.java
@@ -0,0 +1,28 @@
+package com.sample1;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+@Configuration
+@EnableWebMvc
+@EnableTransactionManagement
+@ComponentScan(basePackages="com.sample1")
+//@PropertySource("classpath:database.properties")
+//@EnableJpaRepositories("com.sample1.dao")
+public class WebAppConfig {
+	
+	@Bean
+	public UrlBasedViewResolver setupViewResolver(){
+		UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		resolver.setViewClass(JstlView.class);
+		return resolver;
+	}
+
+}
---
+++ sample1/src/main/java/com/sample1/WebAppInitializer.java
@@ -0,0 +1,25 @@
+package com.sample1;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration.Dynamic;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class WebAppInitializer implements WebApplicationInitializer {
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+		context.register(WebAppConfig.class);
+		servletContext.addListener(new ContextLoaderListener(context));
+		context.setServletContext(servletContext);
+		Dynamic servlet = servletContext.addServlet("dispatcher", new DispatcherServlet(context));
+		servlet.addMapping("/");
+		servlet.setLoadOnStartup(1);
+	}
+
+}

fb4ca12c39872b6892e5cba41f5b753a95206b02
Nick <yashaev@gmail.com>
2015-08-09 11:24:29 +0300

    cleaning the webbappp Controller

--- sample1/src/main/java/com/sample1/controller/WebAppController.java
+++ sample1/src/main/java/com/sample1/controller/WebAppController.java
@@ -17,66 +17,24 @@ import com.sample1.model.User;
 import com.sample1.service.UserService;
 
 @Controller
-//@RequestMapping("/")
+
 public class WebAppController {
 
 	@Autowired(required = true )
 	private UserService userService;
 	
-	/*
-	@Autowired
-	private DataSource datasource;
-	*/
-	
-	/*
-	@RequestMapping(method = RequestMethod.GET)
-	public String sayHello(ModelMap model) {
-		model.addAttribute("greeting", "Hello World from Spring 4 MVC");
-		return "welcome";
-	}
-
-	@RequestMapping(value = "/helloagain", method = RequestMethod.GET)
-	public String sayHelloAgain(ModelMap model) {
-		model.addAttribute("greeting", "Hello World Again, from Spring 4 MVC");
-		return "welcome";
-	}
-	*/
-	
-	
-	@RequestMapping(value = "/get", method = RequestMethod.GET)
+	@RequestMapping(value = "/user", method = RequestMethod.GET)
 	@ResponseStatus(HttpStatus.OK)
 	public @ResponseBody  List<User> get() {
 		return userService.getAllUsers();
-		
-		
-/*
-		
-		try {
-			Connection  con = datasource.getConnection();
-			System.out.println("we got a fking connection ! ");
-			
-			String selectSQL = "SELECT * FROM `users`";
-			PreparedStatement preparedStatement = con.prepareStatement(selectSQL);
-			ResultSet rs = preparedStatement.executeQuery(selectSQL );
-
-			while (rs.next()) {
-				String id = rs.getString("id");
-				String fname = rs.getString("first_name");	
-				String lname = rs.getString("last_name");	
-				String address = rs.getString("address");	
-				
-				System.out.println( id + fname  + lname + address);
-			}
-			
-			con.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		*/
 	}
 
-	
+//	@RequestMapping(value = "/user/{id}", method = RequestMethod.GET)
+//	@ResponseStatus(HttpStatus.OK)
+//	public @ResponseBody  User get(@Parm id) {
+//		return userService.getUsers(id);
+//	}
+
 	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
 	public String home() {
 		System.out.println("HomeController: Passing through...");
